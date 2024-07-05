6c32e31d24f54b93d9d3cfab68e67be09d049603
Varun <varun.palanisamy@gmail.com>
2024-07-05 14:46:31 -0700

    Added send email function, added subject and sends email of the top 20 articles for Tesla

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import requests
+from send_email import send_email
+
 
 api_key = "3f465e6b8aab4494af8ac55ae4447f32"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-06-05&sortBy=publishedAt&apiKey=3f465e6b8aab4494af8ac55ae4447f32"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2024-06-05&sortBy=publishedAt&apiKey=3f465e6b8aab4494af8ac55ae4447f32&language=en"
 
 request = requests.get(url)
 content = request.json()
 
+body = ""
+i = 0
+for article in content["articles"][:20]:
+    title = article.get("title", "No Title")
+    description = article.get("description", "No Description")
+    if i==0:
+        body += "Subject: Today's news" + "\n" + f"{title}\n{description}" + "\n" + article["url"] + "\n\n"
+        i=1
+    else:
+        body += f"{title}\n{description}" + "\n" + article["url"] + "\n\n"
 
-# print(content["articles"]["titles"])
-
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
-
+body =body.encode("utf-8")
+send_email(message = body)
---
+++ send_email.py
@@ -0,0 +1,21 @@
+import smtplib
+import ssl
+
+
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "varun.palanisamy@gmail.com"
+    password = "ocwqxuzzluktntpn"
+    reveiver = "varun.palanisamy@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, reveiver, message)
+
+
+
+
